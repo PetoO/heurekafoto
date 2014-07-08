a85cbe363883750962a2b9c827fed2a1897286af
fadawar <fadawar@gmail.com>
2014-07-08 22:40:41 +0200

    Funkcne riesenie. Nerobi to ale unikatne. Upraveny rspec aby nehadzal warning

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,29 @@
+require 'net/http'
+
 class Offer < ActiveRecord::Base
+  belongs_to :camera
+
+  def self.parse_camera_from_heureka(camera_name)
+    parse_url = url_from_name(camera_name)
+    uri = URI(parse_url)
+    html = Net::HTTP.get(uri)
+    doc = Nokogiri::HTML.parse(html)
+
+    doc.search('#offers .shopspr .shoppr').each do |div|
+      anchor = div.search('.buy .link').first
+      price = div.search('.pr .pricen').text.to_f
+      Offer.create!(
+        shop: anchor.text,
+        url: anchor[:href],
+        price: price
+      )
+    end
+
+  end
+
+  def self.url_from_name(name)
+    url = 'http://digitalne-fotoaparaty.heureka.sk/'
+    url += name.downcase.strip.gsub ' ', '-'
+    url + '/'
+  end
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Offer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'convert cameras name to url' do
+    expect(Offer.url_from_name('Nikon D3200')).to eq('http://digitalne-fotoaparaty.heureka.sk/nikon-d3200/')
+    expect(Offer.url_from_name('Sony Cyber-Shot DSC-RX100')).to eq('http://digitalne-fotoaparaty.heureka.sk/sony-cyber-shot-dsc-rx100/')
+    expect(Offer.url_from_name('Canon PowerShot SX510 HS')).to eq('http://digitalne-fotoaparaty.heureka.sk/canon-powershot-sx510-hs/')
+  end
+
+  it 'parses offers' do
+    Offer.parse_camera_from_heureka 'Nikon D3200'
+
+    expect(Offer.count).not_to eq(0)
+
+    offer = Offer.first
+
+    expect(offer.price).to eq(389.00)
+    expect(offer.shop).to eq('MALL.SK')
+    expect(offer.url).to eq('http://www.heureka.sk/exit/mall-sk/749411912/?z=2&p=1')
+  end
 end
